6eaeb52e510f083dec75531cf58a668319394ddc
Izabela <104584812+izabela-jelovac@users.noreply.github.com>
2023-05-03 15:10:50 +0200

    add operations and return computation as string

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -48,6 +48,22 @@ function evaluate({ currentOperand, previousOperand, operation }) {
   const prev = parseFloat(previousOperand);
   const current = parseFloat(currentOperand);
   if (isNaN(prev) || isNaN(current)) return "";
+  let computation = "";
+  switch (operation) {
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "*":
+      computation = prev * current;
+      break;
+    case "/":
+      computation = prev / current;
+      break;
+  }
+  return computation.toString();
 }
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
